caefbdfc2ee1a1b87d9bccafcf89173efc9ba775
Juliet Valcheva <j.valcheva26@gmail.com>
2026-02-13 15:48:26 +0200

    SQL  tables

---
+++ movies_project1.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS users(
+user_id BIGSERIAL PRIMARY KEY,
+first_name VARCHAR(100) NOT NULL,
+second_name VARCHAR(100) NOT NULL,
+phone_number VARCHAR(15),
+email VARCHAR(100) UNIQUE NOT NULL,
+password VARCHAR(100) NOT NULL,
+created_on TIMESTAMP NOT NULL,
+last_login TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS movies (
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    genre VARCHAR(100),
+    year INTEGER
+);
+
+ CREATE TABLE IF NOT EXISTS reviews (
+    id BIGSERIAL PRIMARY KEY,
+    movie_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    rating DOUBLE CHECK(rating >= 0 AND rating <= 10),
+    review TEXT,
+	 FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE,
+	 FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+UPDATE users
+SET last_login= CURRENT_TIMESTAMP
+WHERE last_login IS NULL;
